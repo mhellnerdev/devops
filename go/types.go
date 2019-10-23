d70dbc20200e5d8c6e9ca51db77606fa7a26c063
Mark Hellner <mhellnerdev@gmail.com>
2019-10-23 00:26:05 -0400

    add

---
+++ go/types.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	var name string
+	name = "Destinee"  // when see quotes, think string
+
+	var character rune // converts charactr to ASCII value
+	character = 'a'
+
+	fmt.Println(character)
+	
+}
