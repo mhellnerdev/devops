5539d6419dbcea18cca222b4ced75389d7ab8c6b
Mark Hellner <mhellnerdev@gmail.com>
2019-11-01 01:35:51 -0400

    updated comments on ec2 terraform

--- terraform/aws/ec2/simple/ec2.tf
+++ terraform/aws/ec2/simple/ec2.tf
@@ -1,23 +1,28 @@
+/* Declare provider and region */
 
 provider "aws" {
   profile    = "default"
   region     = var.region
 }
 
+/* Declare S3 and DynamoDB backend */
+
 terraform {
   backend "s3" {
     bucket = "circlelabs-terraform-states"
-    key = "terraform-state"
+    key = "ec2-simple.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform-lock"
   }
 }
 
+/* Declare AWS EC2 instance */
+
 resource "aws_instance" "terraform-01" {
-  ami           = "ami-00dc79254d0461090"
+  ami           = "ami-00dc79254d0461090" // amazon linux
   instance_type = "t2.micro"
   key_name      = var.useastkeyname
-  vpc_security_group_ids = ["sg-002d15a754884e25e"]
+  vpc_security_group_ids = ["sg-002d15a754884e25e"] // WebDMZ
   tags = {
   Name = "Terraform-01"
   }
@@ -29,6 +34,8 @@ resource "aws_instance" "terraform-01" {
     encrypted = true
   }
 
+/* Remote connectection and setup. */
+
   connection {
     type     = "ssh"
     user     = "ec2-user"
@@ -36,41 +43,27 @@ resource "aws_instance" "terraform-01" {
     host     = self.public_ip
   }
 
+/* Remote commands */
+
   provisioner "remote-exec" {
     inline = [
-      "sudo yum update -y",
-      
+      "sudo yum update -y"
     ]
   }
- provisioner "file" {
-    source = "~/SSH/mhellnerdev-us-east-kp.pem"
-    destination = "/home/ec2-user/.ssh/mhellnerdev-us-east-kp.pem"
+
+/*  Copy private key to new server. Must run terrform apply as sudo */      
+    
+    provisioner "file" {
+      source = "~/SSH/mhellnerdev-us-east-kp.pem"
+      destination = "/home/ec2-user/.ssh/mhellnerdev-us-east-kp.pem"
   }
+
 }
   
-
+/* Declare and assign elastic IP to instance */
+ 
 resource "aws_eip" "terraform-ip" {
     vpc = true
     instance = aws_instance.terraform-01.id
-
 }
 
-/*
-resource "aws_instance" "terraform-02" {
-  ami           = "ami-00dc79254d0461090"
-  instance_type = "t2.micro"
-  key_name      = "mhellnerdev-us-east-kp"
-  vpc_security_group_ids = ["sg-002d15a754884e25e"]
-  tags = {
-  Name = "TerraForm-02"
-  }
-  
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = "8"
-    delete_on_termination = true
-    encrypted = true
-  }
-}
-
-*/
