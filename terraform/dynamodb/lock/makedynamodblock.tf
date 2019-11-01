436ef43f99050e18fe35010801fd722f08475927
Mark Hellner <mhellnerdev@gmail.com>
2019-10-31 23:46:57 -0400

    added dynamodb locking

---
+++ terraform/dynamodb/lock/makedynamodblock.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  profile    = "default"
+  region     = var.region
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-lock"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
--- terraform/ec2/simple/ec2.tf
+++ terraform/ec2/simple/ec2.tf
@@ -9,6 +9,7 @@ terraform {
     bucket = "circlelabs-terraform-states"
     key = "terraform-state"
     region = "us-east-1"
+    dynamodb_table = "terraform-lock"
   }
 }
 
