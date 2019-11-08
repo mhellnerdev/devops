b5aeb9fea12e1d5d73c4eafc1e6b022b1dbe5614
Mark Hellner <mhellnerdev@gmail.com>
2019-11-08 03:23:40 -0500

    added inline bash script to install ansible master node

--- terraform/aws/ec2/ansible-env/terraform.tf
+++ terraform/aws/ec2/ansible-env/terraform.tf
@@ -45,15 +45,15 @@ resource "aws_instance" "ansible-control-node" {
     host     = self.public_ip
   }
 
-/* Remote commands */
+/* Remote commands - Install Ansible and dependencies */
 
   provisioner "remote-exec" {
     inline = [
       "sudo apt update -y",
-      "sudo apt-get install software-properties-common",
-      "sudo apt-add-repository ppa:ansible/ansible",
-      "sudo apt-get update",
-      "sudo apt-get install ansible"
+      "sudo apt-get install software-properties-common -y",
+      "sudo apt-add-repository ppa:ansible/ansible -y",
+      "sudo apt-get update -y",
+      "sudo apt-get install ansible -y"
     ]
   }
 
@@ -76,163 +76,163 @@ resource "aws_eip" "terraform-ip" {
 
 ############################################################
 
-/* Declare AWS EC2 INVENTORY NODE 01 */
+# /* Declare AWS EC2 INVENTORY NODE 01 */
 
-resource "aws_instance" "ansible-inventory-01" {
-  ami           = "ami-04b9e92b5572fa0d1" // ubuntu linux
-  instance_type = "t2.micro"
-  key_name      = var.useastkeyname
-  vpc_security_group_ids = ["sg-002d15a754884e25e"] // WebDMZ
-  tags = {
-  Name = "ansible-inventory-01"
-  }
+# resource "aws_instance" "ansible-inventory-01" {
+#   ami           = "ami-04b9e92b5572fa0d1" // ubuntu linux
+#   instance_type = "t2.micro"
+#   key_name      = var.useastkeyname
+#   vpc_security_group_ids = ["sg-002d15a754884e25e"] // WebDMZ
+#   tags = {
+#   Name = "ansible-inventory-01"
+#   }
 
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = "8"
-    delete_on_termination = true
-    encrypted = false
-  }
+#   root_block_device {
+#     volume_type = "gp2"
+#     volume_size = "8"
+#     delete_on_termination = true
+#     encrypted = false
+#   }
 
-/* Remote connectection and setup. */
+# /* Remote connectection and setup. */
 
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file(var.useastprvkey)
-    host     = self.public_ip
-  }
+#   connection {
+#     type     = "ssh"
+#     user     = "ubuntu"
+#     private_key = file(var.useastprvkey)
+#     host     = self.public_ip
+#   }
 
-/* Remote commands */
+# /* Remote commands */
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y"
-    ]
-  }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo apt update -y"
+#     ]
+#   }
 
-/*  Copy private key to new server. Must run terrform apply as sudo */      
+# /*  Copy private key to new server. Must run terrform apply as sudo */      
     
-    provisioner "file" {
-      source = "~/SSH/mhellnerdev-us-east-kp.pem"
-      destination = "/home/ubuntu/.ssh/mhellnerdev-us-east-kp.pem"
-  }
+#     provisioner "file" {
+#       source = "~/SSH/mhellnerdev-us-east-kp.pem"
+#       destination = "/home/ubuntu/.ssh/mhellnerdev-us-east-kp.pem"
+#   }
 
-}
+# }
   
-/* Declare and assign elastic IP to instance
+# /* Declare and assign elastic IP to instance
  
-resource "aws_eip" "terraform-ip" {
-    vpc = true
-    instance = aws_instance.terraform-01.id
-} 
-*/
+# resource "aws_eip" "terraform-ip" {
+#     vpc = true
+#     instance = aws_instance.terraform-01.id
+# } 
+# */
 
-#############################################################
+# #############################################################
 
-/* Declare AWS EC2 INVENTORY NODE 02 */
+# /* Declare AWS EC2 INVENTORY NODE 02 */
 
-resource "aws_instance" "ansible-inventory-02" {
-  ami           = "ami-04b9e92b5572fa0d1" // ubuntu linux
-  instance_type = "t2.micro"
-  key_name      = var.useastkeyname
-  vpc_security_group_ids = ["sg-002d15a754884e25e"] // WebDMZ
-  tags = {
-  Name = "ansible-inventory-02"
-  }
+# resource "aws_instance" "ansible-inventory-02" {
+#   ami           = "ami-04b9e92b5572fa0d1" // ubuntu linux
+#   instance_type = "t2.micro"
+#   key_name      = var.useastkeyname
+#   vpc_security_group_ids = ["sg-002d15a754884e25e"] // WebDMZ
+#   tags = {
+#   Name = "ansible-inventory-02"
+#   }
 
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = "8"
-    delete_on_termination = true
-    encrypted = false
-  }
+#   root_block_device {
+#     volume_type = "gp2"
+#     volume_size = "8"
+#     delete_on_termination = true
+#     encrypted = false
+#   }
 
-/* Remote connectection and setup. */
+# /* Remote connectection and setup. */
 
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file(var.useastprvkey)
-    host     = self.public_ip
-  }
+#   connection {
+#     type     = "ssh"
+#     user     = "ubuntu"
+#     private_key = file(var.useastprvkey)
+#     host     = self.public_ip
+#   }
 
-/* Remote commands */
+# /* Remote commands */
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y"
-    ]
-  }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo apt update -y"
+#     ]
+#   }
 
-/*  Copy private key to new server. Must run terrform apply as sudo */      
+# /*  Copy private key to new server. Must run terrform apply as sudo */      
     
-    provisioner "file" {
-      source = "~/SSH/mhellnerdev-us-east-kp.pem"
-      destination = "/home/ubuntu/.ssh/mhellnerdev-us-east-kp.pem"
-  }
+#     provisioner "file" {
+#       source = "~/SSH/mhellnerdev-us-east-kp.pem"
+#       destination = "/home/ubuntu/.ssh/mhellnerdev-us-east-kp.pem"
+#   }
 
-}
+# }
   
-/* Declare and assign elastic IP to instance
+# /* Declare and assign elastic IP to instance
  
-resource "aws_eip" "terraform-ip" {
-    vpc = true
-    instance = aws_instance.terraform-01.id
-} 
-*/
+# resource "aws_eip" "terraform-ip" {
+#     vpc = true
+#     instance = aws_instance.terraform-01.id
+# } 
+# */
 
 
-############################################################
+# ############################################################
 
-/* Declare AWS EC2 INVENTORY NODE 03 */
+# /* Declare AWS EC2 INVENTORY NODE 03 */
 
-resource "aws_instance" "ansible-db" {
-  ami           = "ami-04b9e92b5572fa0d1" // ubuntu linux
-  instance_type = "t2.micro"
-  key_name      = var.useastkeyname
-  vpc_security_group_ids = ["sg-002d15a754884e25e"] // WebDMZ
-  tags = {
-  Name = "ansible-db"
-  }
+# resource "aws_instance" "ansible-db" {
+#   ami           = "ami-04b9e92b5572fa0d1" // ubuntu linux
+#   instance_type = "t2.micro"
+#   key_name      = var.useastkeyname
+#   vpc_security_group_ids = ["sg-002d15a754884e25e"] // WebDMZ
+#   tags = {
+#   Name = "ansible-db"
+#   }
 
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = "8"
-    delete_on_termination = true
-    encrypted = false
-  }
+#   root_block_device {
+#     volume_type = "gp2"
+#     volume_size = "8"
+#     delete_on_termination = true
+#     encrypted = false
+#   }
 
-/* Remote connectection and setup. */
+# /* Remote connectection and setup. */
 
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file(var.useastprvkey)
-    host     = self.public_ip
-  }
+#   connection {
+#     type     = "ssh"
+#     user     = "ubuntu"
+#     private_key = file(var.useastprvkey)
+#     host     = self.public_ip
+#   }
 
-/* Remote commands */
+# /* Remote commands */
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y"
-    ]
-  }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo apt update -y"
+#     ]
+#   }
 
-/*  Copy private key to new server. Must run terrform apply as sudo */      
+# /*  Copy private key to new server. Must run terrform apply as sudo */      
     
-    provisioner "file" {
-      source = "~/SSH/mhellnerdev-us-east-kp.pem"
-      destination = "/home/ubuntu/.ssh/mhellnerdev-us-east-kp.pem"
-  }
+#     provisioner "file" {
+#       source = "~/SSH/mhellnerdev-us-east-kp.pem"
+#       destination = "/home/ubuntu/.ssh/mhellnerdev-us-east-kp.pem"
+#   }
 
-}
+# }
   
-/* Declare and assign elastic IP to instance
+# /* Declare and assign elastic IP to instance
  
-resource "aws_eip" "terraform-ip" {
-    vpc = true
-    instance = aws_instance.terraform-01.id
-} 
-*/
+# resource "aws_eip" "terraform-ip" {
+#     vpc = true
+#     instance = aws_instance.terraform-01.id
+# } 
+# */
