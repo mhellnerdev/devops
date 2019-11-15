aa7651afd7f96b180c887e77ea2d63ee8d63d4a4
Mark Hellner <mhellnerdev@gmail.com>
2019-11-15 14:05:27 -0500

    added docker terraform deployment

---
+++ terraform/aws/ec2/dockerhost/terraform.tf
@@ -0,0 +1,76 @@
+/* Declare provider and region */
+
+provider "aws" {
+  profile    = "default"
+  region     = var.region
+}
+
+/* Declare S3 and DynamoDB backend */
+
+terraform {
+  backend "s3" {
+    bucket = "circlelabs-terraform-states" // .tfstate file gets stored in S3
+    key = "ec2-dockerhost.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-lock" // dynamodb lock that prevents modification of .tf file while in and UP state
+  }
+}
+
+############################################################
+/* Declare AWS EC2 Docker Host */
+############################################################
+
+resource "aws_instance" "docker-host" {
+  ami           = var.ami // ubuntu linux
+  instance_type = var.instancetype
+  key_name      = var.useastkeyname
+  vpc_security_group_ids = ["sg-002d15a754884e25e"] // WebDMZ
+  tags = {
+  Name = "docker-host"
+  }
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "8"
+    delete_on_termination = true
+    encrypted = false
+  }
+
+/* Remote connectection and setup. */
+
+  connection {
+    type     = "ssh"
+    user     = var.username
+    private_key = file(var.useastprvkey)
+    host     = self.public_ip
+  }
+
+/* Remote commands - Install Ansible and dependencies */
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt-get install software-properties-common -y",
+      "sudo apt-add-repository ppa:ansible/ansible -y",
+      "sudo apt-get update -y",
+      "sudo apt-get install ansible -y",
+      "sudo apt-get install docker.io -y",
+    ]
+  }
+
+/*  Copy private key to new server. Must run terrform apply as sudo */      
+    
+    provisioner "file" {
+      source = var.useastprvkey
+      destination = "/home/ubuntu/.ssh/mhellnerdev-us-east-kp.pem"
+  }
+
+}
+  
+/* Declare and assign elastic IP to instance
+ 
+resource "aws_eip" "terraform-ip" {
+    vpc = true
+    instance = aws_instance.terraform-01.id
+} 
+*/
---
+++ terraform/aws/ec2/dockerhost/variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "useastkeyname" {
+    default      = "mhellnerdev-us-east-kp"
+  }
+
+variable "useastprvkey" {
+     default = "~/SSH/mhellnerdev-us-east-kp.pem"
+}
+
+variable "instancetype" {
+    default = "t2.micro"
+}
+
+variable "ami" {
+    default = "ami-04b9e92b5572fa0d1"
+}
+
+variable "username" {
+    default = "ubuntu"
+}
+
+
