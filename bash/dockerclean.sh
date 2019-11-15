c4b33c8016f38c79b8f5c2e2b196f048e441be4f
Mark Hellner <mhellnerdev@gmail.com>
2019-11-15 00:58:56 -0500

    docker nginx and bash added

---
+++ bash/dockerclean.sh
@@ -0,0 +1,11 @@
+docker rm $(docker ps -aq -f 'status=exited')
+
+echo "########################################"
+echo "####### Cleaned Containers #############"
+echo "########################################"
+
+docker rmi $(docker images -f 'dangling=true' -q)
+
+echo "########################################"
+echo "####### Cleaned Dangling Images ########"
+echo "########################################"
