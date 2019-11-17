93fb0a4c62dc43e51b1d67fbc478cded8f1d0228
Mark Hellner <mhellnerdev@gmail.com>
2019-11-17 18:29:44 -0500

    add go

---
+++ go/functions.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+//
+func circleLabs() {
+	fmt.Println("Circle Labs")
+	fmt.Println("Automated Infrastructure Management")
+	fmt.Println("Since 2020")
+}
+
+func main() {
+	circleLabs()
+	circleLabs()
+	circleLabs()
+	circleLabs()
+
+}
