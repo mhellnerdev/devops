ff2fd0215876a8f3af70c315b9ee39790bd02cfb
Mark Hellner <mhellnerdev@gmail.com>
2022-07-30 00:03:35 -0400

    add python environment install script

---
+++ bash/setuppython.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install python3 -y
+sudo apt install python-is-python3 -y
+sudo apt install python3-pip -y
+sudo apt install python3.8-venv -y
