cb350ed01956c2b47fc6a9cfc0c088a5a312c549
mhellnerdev <mhellnerdev@gmail.com>
2022-07-31 20:11:44 -0400

    add ssh-key output

--- bash/gitconfig.sh
+++ bash/gitconfig.sh
@@ -5,5 +5,7 @@ git config --global user.name "mhellnerdev"
 
 ssh-keygen -t ed25519 -f ~/.ssh/github -C "mhellnerdev@gmail.com" 
 
+cat ~/.ssh/github.pub
+
 echo 'eval $(ssh-agent -s) > /dev/null' >> ~/.bashrc
 echo 'ssh-add ~/.ssh/github 2> /dev/null' >> ~/.bashrc
