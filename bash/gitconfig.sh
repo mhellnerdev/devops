f4bcc92c6ae1fa989c7c66df4f213ebd917280bd
mhellnerdev <mhellnerdev@gmail.com>
2022-07-31 20:08:51 -0400

    generate key and add to .bashrc

--- bash/gitconfig.sh
+++ bash/gitconfig.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 
 git config --global user.email "mhellnerdev@gmail.com"
-git config --global user.name "Mark Hellner"
+git config --global user.name "mhellnerdev"
+
+ssh-keygen -t ed25519 -f ~/.ssh/github -C "mhellnerdev@gmail.com" 
+
+echo 'eval $(ssh-agent -s) > /dev/null' >> ~/.bashrc
+echo 'ssh-add ~/.ssh/github 2> /dev/null' >> ~/.bashrc
