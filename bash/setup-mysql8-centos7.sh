5b6142e0d44b7702d072bbbb79848a2a08d87acb
mhellnerdev <mhellnerdev@gmail.com>
2022-11-12 02:22:26 -0500

    add setup-mysql.sh

---
+++ bash/setup-mysql8-centos7.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install mysql on centos7
+sudo yum update -y
+sudo yum install wget -y
+wget https://repo.mysql.com/mysql80-community-release-el7-1.noarch.rpm
+sudo yum localinstall mysql80-community-release-el7-1.noarch.rpm -y
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+sudo yum install mysql-community-server -y
+
