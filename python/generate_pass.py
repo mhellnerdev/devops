39f4b5d40ab047763bd176ded91577b0d41bd8a4
mhellnerdev <mhellnerdev@gmail.com>
2022-11-20 09:32:40 -0500

    add password generator

---
+++ python/generate_pass.py
@@ -0,0 +1,13 @@
+import random
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+symbols = "@#$%&*/\?"
+
+use_for = lower_case + upper_case + number + symbols
+length_for_pass = 10
+
+password = "".join(random.sample(use_for, length_for_pass))
+
+print(f"Your generated password is: {password}")
