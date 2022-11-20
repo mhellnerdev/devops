227d77f29ac257c9dd30e7b42ba7f73d6d0e7b60
mhellnerdev <mhellnerdev@gmail.com>
2022-11-20 11:00:21 -0500

    add list of fun terminal applications

---
+++ bash/boredom.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# shows os detailed info with ascii art
+sudo apt install neofetch
+
+# install steam locomotive in terminal
+sudo apt install sl
+
+# visit the matrix
+sudo apt install cmatrix
+
+# fire in the terminal. run with "aafire"
+sudo apt install libaa-bin
+
+# fortune teller
+sudo apt install fortune
+
+# pet in terminal
+sudo apt install oneko
+
+# talking cow. useage "cowsay <'what does the cow say?'>"
+sudo apt install cowsay
+
+# lolcat colored gradient terminal. pipe to lolcat command. usage <command> | lolcat
+sudo aptm install lolcat
+
+# watch star wars over telnet
+telnet towel.blinkenlights.nl
+
