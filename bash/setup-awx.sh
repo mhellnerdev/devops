6129d0727b8f9ce16a29de9f048c6de2f6b84922
mhellnerdev <mhellnerdev@gmail.com>
2023-01-06 12:33:47 -0500

    updates

--- bash/setup-awx.sh
+++ bash/setup-awx.sh
@@ -13,3 +13,41 @@ sudo hostnamectl set-hostname awx-stream
 vim /etc/hosts
 sudo init 6
 
+# install k3s
+curl -sfL https://get.k3s.io | sudo bash -
+sudo su -
+kubectl get nodes
+
+# Ingest yaml file that builds awx-operator pods
+kubectl apply -f https://raw.githubusercontent.com/ansible/awx-operator/0.13.0/deploy/awx-operator.yaml
+
+kubectl get pods -A
+
+# Get logs of awx-operator pod
+kubectl logs -f awx-operator-xxxxxxxxxxx
+
+# Create AWX config
+cat <<EOF | sudo tee awx.yaml
+---
+apiVersion: awx.ansible.com/v1beta1
+kind: AWX
+metadata:
+  name: awx
+spec:
+  service_type: nodeport
+  ingress_type: none
+  hostname: awx-stream.circlelabs.home  
+EOF
+
+# Ingest awx.yaml config. This step stands up ansible.
+kubectl apply -f awx.yaml
+
+# Get logs of awx-operator pod
+kubectl logs -f awx-operator-xxxxxxxxxxx
+
+# check all pods are up and running
+kubectl get pods -A
+
+# get network info about pods
+kubectl get svc
+
