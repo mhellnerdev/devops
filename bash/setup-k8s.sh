26665dde7e76dfaa5f53d9fdda5e41d69f2643f5
mhellnerdev <mhellnerdev@gmail.com>
2023-01-25 23:25:37 -0500

    add awx

--- bash/setup-k8s.sh
+++ bash/setup-k8s.sh
@@ -184,3 +184,76 @@ kubectl apply -f dashboard-adminuser.yaml
 # get token to login to dashboard
 kubectl -n kubernetes-dashboard create token admin-user
 
+
+
+
+###############
+##### AWX #####
+###############
+
+# install kustomize
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+sudo mv kustomize /usr/local/bin
+
+# setup kustomize
+cat <<EOF | tee kustomization.yaml
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+  # Find the latest tag here: https://github.com/ansible/awx-operator/releases
+  - github.com/ansible/awx-operator/config/default?ref=1.1.3
+
+# Set the image tags to match the git version from above
+images:
+  - name: quay.io/ansible/awx-operator
+    newTag: 1.1.3
+
+# Specify a custom namespace in which to install AWX
+namespace: awx 
+EOF
+
+# Build AWX operator pod with above yaml
+kustomize build . | kubectl apply -f -
+
+# Set default namespace to awx. Use if you do not want to keep using -n or --namespace
+kubectl config set-context --current --namespace=awx
+
+# See pods all starting, otherwise pass --namespace awx
+kubectl get pods -n awx
+kubectl get pods -A
+
+# Get logs of awx-operator pod
+kubectl logs -f awx-operator-controller-manager-577f6968b5-vqks8
+
+# Create AWX pods yaml
+cat <<EOF | tee awx.yaml
+---
+apiVersion: awx.ansible.com/v1beta1
+kind: AWX
+metadata:
+  name: awx
+spec:
+  service_type: nodeport
+  nodeport_port: 30080 
+EOF
+
+# TODO Add '- awx.yaml' in kustomization.yaml to resources line after the github link.
+
+# Run kustomize build again to apply new included awx.yaml file. This step stands up ansible awx on kubernetes.
+# This may take a while. Watch logs as it comes up with next command
+kustomize build . | kubectl apply -f -
+
+# Watch logs of awx-operator pod
+sudo kubectl logs -f awx-operator-controller-manager-xxxxxxxxxx -c awx-manager --namespace awx
+
+# Check all pods are up and running
+kubectl get pods -n awx
+kubectl get pods -A
+
+# Outputs secret password that is needed to login for the first time.
+kubectl get secret awx-admin-password -o jsonpath="{.data.password}" --namespace awx | base64 --decode
+
+# Get network info about pods. Look for exposed NodePort for awx-service.
+# Should be 30080 as defined in the awx.yaml file setup earlier.
+# Visit hosts IP address with this port to login to AWX for the first time.
+kubectl get svc -A
