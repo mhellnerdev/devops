3147dd7ce9d3632a26905c423261cca9de97d29b
mhellnerdev <mhellnerdev@gmail.com>
2023-02-11 15:47:13 -0500

    remove need for channel declaration.

--- php/send_slack_message.php
+++ php/send_slack_message.php
@@ -1,19 +1,19 @@
 <?php
 
-function send_slack_message($message, $channel = "#general")
+function send_slack_message($message)
 {
-    $slack_webhook_url = "YOUR_SLACK_WEBHOOK_URL";
+    $slack_webhook_url = "<WEBHOOK_URL>";
+    $emoji = ":rotating_light:";
 
-    $data = "payload=" . json_encode(array(
-        "channel"       =>  $channel,
+    $data = array(
         "text"          =>  $message,
-        "icon_emoji"    =>  ":rotating_light:"
-    ));
+        "icon_emoji"    =>  $emoji,
+    );
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $slack_webhook_url);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "payload=" . json_encode($data));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $result = curl_exec($ch);
@@ -23,6 +23,5 @@ function send_slack_message($message, $channel = "#general")
 }
 
 $message = "This is a log alert from PHP script.";
-$channel = "#alerts";
 
-send_slack_message($message, $channel);
+send_slack_message($message);
