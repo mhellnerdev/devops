d87b7c75a94ed6cb9c10302bc79c3ad8d3286dd8
mhellnerdev <mhellnerdev@gmail.com>
2023-02-18 01:50:30 -0500

    update curlcrazy

--- bash/curl-crazy.sh
+++ bash/curl-crazy.sh
@@ -1,4 +1,42 @@
-count=100
-for i in $(seq $count); do
-    curl http://angular-dev-alb-916757676.us-east-1.elb.amazonaws.com
+#!/bin/bash
+
+count=0
+target=""
+
+while getopts ":n:t:" opt; do
+  case $opt in
+    n)
+      count=$OPTARG
+      ;;
+    t)
+      target=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      echo "Usage: $(basename $0) [-n count] [-t target]"
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      echo "Usage: $(basename $0) [-n count] [-t target]"
+      exit 1
+      ;;
+  esac
 done
+
+if [[ $count -eq 0 || -z $target ]]; then
+  echo "Usage: $(basename $0) [-n count] [-t target]"
+  exit 1
+fi
+
+for i in $(seq $count); do
+    if curl -sSf $target >/dev/null; then
+        printf "\rGoing CRAZY with the curls. %d completed." $i
+    else
+        http_code=$(curl -sSf -w "%{http_code}" $target -o /dev/null)
+        echo -e "\nCan't connect to $target. Status code: $http_code"
+        exit 1
+    fi
+done
+
+echo -e "\nThe curls were crazy! You curled $count times!"
