2bce0808be07e66e71a879c611d1d7aff2ea7ad2
mhellnerdev <44537653+mhellnerdev@users.noreply.github.com>
2024-07-10 01:54:38 -0400

    Update and rename setup-rancher.sh to setup-docker-rocky8.sh

--- bash/setup-docker-rocky8.sh
+++ bash/setup-docker-rocky8.sh
@@ -6,14 +6,8 @@ sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/dock
 
 sudo dnf install docker-ce docker-ce-cli containerd.io -y
 
-sudo systemctl start docker
-
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo systemctl status docker
 
 sudo usermod -aG docker $(whoami)
-
-
-
-
